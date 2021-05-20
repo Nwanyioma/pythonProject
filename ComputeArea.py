1b23d1eae64cd0f8919877c29789b9ec44b1ef7e
Nwanyioma <aminahalaka@gmail.com>
2021-05-20 15:55:07 -0700

    first commit

---
+++ ComputeArea.py
@@ -0,0 +1,7 @@
+# Prompt the user to enter a radius
+radius = eval (input("prompt the user to enter a value:"))
+
+# Compute area
+area = radius * radius * 3.14159
+# Display results
+print("The area for the circle of radius", radius, "is", area)
